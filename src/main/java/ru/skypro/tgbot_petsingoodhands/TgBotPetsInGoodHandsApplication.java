886afc54a6698afecc4b9a5733c38edd75555cd9
Zavarka47 <pazuzu1995@gmail.com>
2023-07-19 13:57:16 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/tgbot_petsingoodhands/TgBotPetsInGoodHandsApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.tgbot_petsingoodhands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotPetsInGoodHandsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgBotPetsInGoodHandsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/tgbot_petsingoodhands/TgBotPetsInGoodHandsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.tgbot_petsingoodhands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotPetsInGoodHandsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
