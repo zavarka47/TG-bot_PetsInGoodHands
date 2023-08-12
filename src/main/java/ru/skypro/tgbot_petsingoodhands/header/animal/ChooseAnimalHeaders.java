1d4a251f898135fc1b84ce71a33419fe62aae5d4
Arihin <arihin.slava@yandex.ru>
2023-08-12 19:40:13 +0300

    Классы Headers animal

---
+++ src/main/java/ru/skypro/tgbot_petsingoodhands/header/animal/ChooseAnimalHeaders.java
@@ -0,0 +1,39 @@
+package ru.skypro.tgbot_petsingoodhands.header.animal;
+
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import ru.skypro.tgbot_petsingoodhands.header.TelegramHeader;
+import ru.skypro.tgbot_petsingoodhands.message.Messages;
+
+import java.util.Objects;
+
+public class ChooseAnimalHeaders implements TelegramHeader {
+
+    private final Messages messages;
+
+    public ChooseAnimalHeaders(Messages messages) {
+        this.messages = messages;
+    }
+    @Override
+    public boolean appliesTo(Update update) {
+        return Objects.nonNull(update.message()) && update.message().text().equals("/start");
+    }
+
+    @Override
+    public void handleUpdate(Update update) {
+
+        InlineKeyboardButton button1 = new InlineKeyboardButton("Собаки").callbackData("1");
+        InlineKeyboardButton button2 = new InlineKeyboardButton("Кошки").callbackData("2");
+
+        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup().addRow(button1, button2);
+
+        Long chatId = update.message().chat().id();
+
+        messages.sendMessageWithKeyboard(chatId, """
+                            Привет \\!👋
+                            Какое животное хотел хотите выбрать\\?
+                            """ , keyboard);
+
+    }
+}
